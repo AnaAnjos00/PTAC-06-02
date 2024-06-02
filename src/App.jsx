4e66d75bf90b22e01b411dbc38a77d0ed55dd5df
AnaAnjos00 <ana.silva88@estudante.ifms.edu.br>
2024-06-02 17:19:28 -0300

    Estilização, footer e header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import "./globals.css";
+
 export default function App() {
 
   return(
     <main>
       <Header/>
+      <Footer/>
     </main>
   );
 
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css";
+
+export default function Footer() {
+    return(
+        <footer> 
+            <h1>Footer</h1>
+            Todos os direitos reservados a Ana Anjos
+        </footer>
+    );
+}
