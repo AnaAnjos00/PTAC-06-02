98353f5cae93ed3c149fe2097bba8c8a90183e40
AnaAnjos00 <ana.silva88@estudante.ifms.edu.br>
2024-06-02 16:13:32 -0300

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
+import Header from "./Components/Header";
 export default function App() {
 
-  const nome = "Ana";
-  const sobreNome = "Anjos";
-
   return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-    </div>  
+    <main>
+      <Header/>
+    </main>
   );
 
 
